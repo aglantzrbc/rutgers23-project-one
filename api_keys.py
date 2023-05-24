23756373e206363ea401e4ce727bfd33f896c320
Adam <adam.glantz1@outlook.com>
2023-05-24 17:23:43 -0400

    project 1 module 7 group 7

---
+++ api_keys.py
@@ -0,0 +1,14 @@
+# NYC Data 'regular' API Key:
+api_key_regular = "7lf97f74nqt60adppd7r1nqq6"
+
+# NYC Data 'secret' API Key:
+api_key_secret = "60tzfq44v091n2v4urv8zfvsbjaod581757y1ne1k7svxvbez0"
+
+# NYC Data 'regular' App Token:
+app_token_regular = "llfjtZfD4YZbahRfTY6fpZ8xh"
+
+# NYC Data 'secret' App Token:
+app_token_secret = "YzhGo5l9Tvz3h_JxvJrlHrwy9YhxtLWleg43"
+
+# Pandas info accessible via one of the tabs at the bottom of this page: https://dev.socrata.com/foundry/data.cityofnewyork.us/h9gi-nx95
+
